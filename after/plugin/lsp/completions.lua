0f11ab7bd5e430bc481f56ad8769dc36e517a296
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-05-14 18:05:50 -0600

    prettier formatted

--- after/plugin/lsp/completions.lua
+++ after/plugin/lsp/completions.lua
@@ -1,47 +1,47 @@
 local cmp = require("cmp")
 require("luasnip.loaders.from_vscode").lazy_load()
 cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-o>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.abort(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    -- Super tab
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      local luasnip = require("luasnip")
-      local col = vim.fn.col(".") - 1
-      if cmp.visible() then
-        cmp.select_next_item({ behavior = "select" })
-      elseif luasnip.expand_or_locally_jumpable() then
-        luasnip.expand_or_jump()
-      elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-        fallback()
-      else
-        cmp.complete()
-      end
-    end, { "i", "s" }),
-    -- Super shift tab
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      local luasnip = require("luasnip")
-      if cmp.visible() then
-        cmp.select_prev_item({ behavior = "select" })
-      elseif luasnip.locally_jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  }),
-  snippet = {
-    expand = function(args)
-      require("luasnip").lsp_expand(args.body)
-    end,
-  },
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-  }, {
-    { name = "buffer" },
-  }),
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-o>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		-- Super tab
+		["<Tab>"] = cmp.mapping(function(fallback)
+			local luasnip = require("luasnip")
+			local col = vim.fn.col(".") - 1
+			if cmp.visible() then
+				cmp.select_next_item({ behavior = "select" })
+			elseif luasnip.expand_or_locally_jumpable() then
+				luasnip.expand_or_jump()
+			elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+				fallback()
+			else
+				cmp.complete()
+			end
+		end, { "i", "s" }),
+		-- Super shift tab
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			local luasnip = require("luasnip")
+			if cmp.visible() then
+				cmp.select_prev_item({ behavior = "select" })
+			elseif luasnip.locally_jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	}),
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+	}, {
+		{ name = "buffer" },
+	}),
 })
--- after/plugin/lsp/treesitter.lua
+++ after/plugin/lsp/treesitter.lua
@@ -1,10 +1,10 @@
 local config = require("nvim-treesitter.configs")
 config.setup({
-  ensure_installed = { "go", "lua", "javascript", "c", "rust" },
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+	ensure_installed = { "go", "lua", "javascript", "c", "rust" },
+	sync_install = false,
+	auto_install = true,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
 })
